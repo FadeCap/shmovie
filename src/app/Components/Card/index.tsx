f1bb90d341e28df51cc67cb4ef2963140266d5b4
FadeCap <116163360+FadeCap@users.noreply.github.com>
2025-05-20 11:27:52 +0200

    Parsed more data into Card component, fixed rating to have 2 decimals max

--- src/app/Components/Card/index.tsx
+++ src/app/Components/Card/index.tsx
@@ -4,18 +4,36 @@ import { fetchMovies } from "@/lib/fetchMovies";
 type Movie = {
   id: number;
   original_title: string;
+  vote_average: number;
+  release_date: string;
 };
 async function Card() {
   const movies: Movie[] = await fetchMovies();
-  console.log(movies);
-
+  console.log(movies[0]);
   return (
     <div>
       {movies.map((movie) => (
-        <div key={movie.id} className="border-4 border-teal p-4 rounded-3xl">
-          <h2 className="text-lightgrey text-xl font-bold">
+        <div
+          key={movie.id}
+          className="border-4 border-teal p-4 rounded-3xl text-lightgrey"
+        >
+          <h2 className=" text-xl font-bold flex-row">
             {movie.original_title}
           </h2>
+          {movie.vote_average.toFixed(2)}
+          <div className="image">
+            {
+              //TODO: Parse image from API
+            }
+          </div>
+          <div className="">
+            {
+              //TODO: Unsure if keeping this or making own page for movie details
+            }
+          </div>
+          <div className="text-center p-2">
+            <p>{movie.release_date}</p>
+          </div>
         </div>
       ))}
     </div>
